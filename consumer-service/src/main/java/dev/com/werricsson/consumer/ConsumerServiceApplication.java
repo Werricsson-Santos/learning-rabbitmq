34dc979d16f1292227e1963ca7236b73fe38c182
Werricsson <werricsson.santos@hotmail.com>
2024-08-13 14:26:10 -0300

    Start consumer string service

---
+++ consumer-service/src/main/java/dev/com/werricsson/consumer/ConsumerServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.com.werricsson.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerServiceApplication.class, args);
+	}
+
+}
