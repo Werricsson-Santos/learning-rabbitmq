a951ff58ec63d659082cc9d9fffec3173d1b730e
Werricsson <werricsson.santos@hotmail.com>
2024-08-13 12:06:07 -0300

    First delivery

---
+++ producer-service/src/main/java/dev/com/werricsson/producer/services/StringService.java
@@ -0,0 +1,23 @@
+package dev.com.werricsson.producer.services;
+
+import static dev.com.werricsson.producer.configs.RabbitMQConfig.EXG_NAME_MARKETPLACE;
+import static dev.com.werricsson.producer.configs.RabbitMQConfig.RK_PRODUCT_LOG;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Service
+@RequiredArgsConstructor
+public class StringService {
+	
+	private final RabbitTemplate rabbitTemplate;
+
+	public void produce(String message) {
+		log.info("Received message " + message);
+		rabbitTemplate.convertAndSend(EXG_NAME_MARKETPLACE, RK_PRODUCT_LOG, message);
+	}
+}
