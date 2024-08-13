969dbae43c4dd182e8d99f85eff58797ecabfe1f
Werricsson <werricsson.santos@hotmail.com>
2024-08-13 17:45:09 -0300

    Modifying routing key to common constant

--- consumer-service/src/main/java/dev/com/werricsson/consumer/rabbitmq/consumers/StringConsumer.java
+++ consumer-service/src/main/java/dev/com/werricsson/consumer/rabbitmq/consumers/StringConsumer.java
@@ -5,13 +5,15 @@ import org.springframework.stereotype.Component;
 
 import lombok.extern.log4j.Log4j2;
 
+import static main.java.constants.RabbitMQConstants.RK_PRODUCT_LOG;
 
 @Log4j2
 @Component
 public class StringConsumer {
 
-	@RabbitListener(queues = {"product.log"})
+	@RabbitListener(queues = {RK_PRODUCT_LOG})
 	public void consumer(String message) {
+		
 		log.info("Consumer received a message -> " + message);
 	}
 }
